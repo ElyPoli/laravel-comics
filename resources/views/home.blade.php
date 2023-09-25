9069d130a5b7494326f692ad27c2aa07cf5d8294
ElyPoli <itsos.poli@gmail.com>
2023-09-25 15:18:35 +0200

    created start files

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@vite(['resources/scss/app.scss', 'resources/js/app.js'])
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Esercizio 54: Laravel comics</title>
+
+    <!-- Icona -->
+    <link rel="icon" href="{{ asset('img/favicon.ico') }}">
+</head>
+
+<body>
+</body>
+
+</html>
