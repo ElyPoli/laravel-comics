fcbe8ceb8190d9c4b5e384ac90bc0b9f521e6851
ElyPoli <itsos.poli@gmail.com>
2023-09-25 17:37:26 +0200

    update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -3,20 +3,41 @@
     <div class="top-footer">
         <div class="container d-flex align-items-start justify-content-between">
             <!-- Menù -->
-            <div class="d-flex align-items-start justify-content-between pb-3 pt-3">
-                @foreach ($footerLinks as $link)
-                    <div class="pe-4 ps-4">
-                        <a href="/"
-                            class="link-underline link-underline-opacity-0 text-uppercase text-white text-nowrap fw-bold fs-6 m-0 p-0 pt-4 pb-3">{{ $link['title'] }}</a>
-                        @foreach ($link['subheading'] as $subheading)
-                            <p>{{ $subheading }}</p>
-                        @endforeach
-                    </div>
-                @endforeach
+            <div class="d-flex align-items-start justify-content-between pb-3">
+                <div class="pe-4">
+                    <h3 class="text-uppercase text-white text-nowrap fw-bold fs-6 m-0 p-0 pt-4 pb-3">
+                        {{ $footerLinks[0]['title'] }}</h3>
+                    @foreach ($footerLinks[0]['subheading'] as $subheading)
+                        <p>{{ $subheading }}</p>
+                    @endforeach
+                    <h3 class="text-uppercase text-white text-nowrap fw-bold fs-6 m-0 p-0 pt-4 pb-3">
+                        {{ $footerLinks[1]['title'] }}</h3>
+                    @foreach ($footerLinks[1]['subheading'] as $subheading)
+                        <p>{{ $subheading }}</p>
+                    @endforeach
+                </div>
+                <div class="pe-4 ps-4">
+                    <h3 class="text-uppercase text-white text-nowrap fw-bold fs-6 m-0 p-0 pt-4 pb-3">
+                        {{ $footerLinks[2]['title'] }}</h3>
+                    @foreach ($footerLinks[2]['subheading'] as $subheading)
+                        <p>{{ $subheading }}</p>
+                    @endforeach
+                </div>
+                <div class="ps-4">
+                    <h3 class="text-uppercase text-white text-nowrap fw-bold fs-6 m-0 p-0 pt-4 pb-3">
+                        {{ $footerLinks[3]['title'] }}</h3>
+                    @foreach ($footerLinks[3]['subheading'] as $subheading)
+                        <p>{{ $subheading }}</p>
+                    @endforeach
+                </div>
             </div>
 
             <!-- Logo -->
             <img class="footer-logo" src="{{ asset('img/dc-logo-bg.png') }}" alt="DC logo">
+        </div>
+        <div class="container my-footer-copyright">
+            <p>All Site Content TM and © 2020 DC Entertainment, unless otherwise <span>noted here</span>. All right
+                reserved. <span>Cookies Settings</span></p>
         </div>
     </div>
 
